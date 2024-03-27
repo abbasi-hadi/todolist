9a86bbae046497abea213a3eb980506c04cfe617
abbasi-hadi <abbasi.hadi.2012@gmail.com>
2024-03-27 03:52:13 +0330

    change 404 page

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function Custom404() {
         404
       </h1>
       <h2 className="text-4xl font-bold text-center text-white   p-4 drop-shadow-xl">
-        چیزی پیدا نکردیم!!!
+        چیزی پیدا نکردیم!
       </h2>
       <h6 className="text-xl mt-4 font-normal text-center text-yellow-300"><Link className="" href='/'>بازگشت به صفحه مدیریت وظایف</Link></h6>
     </div>)
